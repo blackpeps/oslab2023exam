7a158cc2d34311a99b83545125dc020d18794e79
blackpeps <126700907+blackpeps@users.noreply.github.com>
2023-08-09 20:55:45 +0530

    Removed unnecessary functions [reqRes()] and variables in Bankers Algorithm

--- 08 - Bankers Algorithm.c
+++ 08 - Bankers Algorithm.c
@@ -1,11 +1,10 @@
-
 #include <stdio.h>
 struct pro
 {
     int all[10], max[10], need[10];
     int flag;
 };
-int i, j, pno, r, nr, id, k = 0, safe = 0, exec, count = 0, wait = 0, max_err = 0;
+int i, j, pno, r, id, k = 0, safe = 0, exec, count = 0, wait = 0;
 struct pro p[10];
 int aval[10], seq[10];
 void safeState()
@@ -14,7 +13,6 @@ void safeState()
     {
         safe = 0;
         for (i = 0; i < pno; i++)
-        {
             if (p[i].flag)
             {
                 exec = r;
@@ -37,7 +35,7 @@ void safeState()
                     count++;
                 }
             }
-        }
+
         if (!safe)
         {
             printf("System is in Unsafe State\n");
@@ -53,115 +51,53 @@ void safeState()
         printf("\n\n");
     }
 }
-void reqRes()
+int main()
 {
-    printf("\nRequest for new Resourses");
-    printf("\nProcess id ? ");
-    scanf("%d", &id);
-    printf("Enter new Request details ");
-    for (i = 0; i < r; i++)
-    {
-
-        scanf("%d", &nr);
-        if (nr <= p[id].need[i])
-        {
-            if (nr <= aval[i])
-            {
-                aval[i] -= nr;
-                p[id].all[i] += nr;
-                p[id].need[i] -= nr;
-            }
-            else
-                wait = 1;
-        }
-        else
-            max_err = 1;
-    }
-    if (!max_err && !wait)
-        safeState();
-    else if (max_err)
-    {
-        printf("\nProcess has exceeded its maximum usage \n");
-    }
-    else
-    {
-        printf("\nProcess need to wait\n");
-    }
-}
-void main()
-{
-
-    printf("Enter no of process ");
+    printf("Enter no of process: ");
     scanf("%d", &pno);
 
-    printf("Enter no. of resourses ");
+    printf("Enter no. of resources: ");
     scanf("%d", &r);
 
-    printf("Enter Available Resourse of each type ");
+    printf("Enter Available Resource of each type: ");
     for (i = 0; i < r; i++)
-    {
         scanf("%d", &aval[i]);
-    }
 
-    printf("\n\n---Resourse Details---");
+    printf("\n---Resource Details---");
     for (i = 0; i < pno; i++)
     {
-
-        printf("\nResourses for process %d\n", i);
-        printf("\nAllocation Matrix\n");
+        printf("\nProcess %d\n", i);
+        printf("Allocation Matrix:\t\t");
         for (j = 0; j < r; j++)
-        {
             scanf("%d", &p[i].all[j]);
-        }
-        printf("Maximum Resourse Request \n");
+
+        printf("Maximum Resource Request:\t");
         for (j = 0; j < r; j++)
-        {
             scanf("%d", &p[i].max[j]);
-        }
+
         p[i].flag = 1;
     }
-    // Calcualting need
-    for (i = 0; i < pno; i++)
-    {
-        for (j = 0; j < r; j++)
-        {
-            p[i].need[j] = p[i].max[j] - p[i].all[j];
-        }
-    }
 
-    // Print Current Details
+    for (i = 0; i < pno; i++)
+        for (j = 0; j < r; j++)
+            p[i].need[j] = p[i].max[j] - p[i].all[j];
+
     printf("\nProcess Details\n");
-    printf("Pid\t\tAllocattion\t\tMax\t\tNeed\n");
+    printf("Pid\t\tAllocation\t\tMax\t\tNeed\n");
     for (i = 0; i < pno; i++)
     {
         printf("%d\t\t", i);
         for (j = 0; j < r; j++)
-        {
             printf("%d  ", p[i].all[j]);
-        }
         printf("\t\t");
         for (j = 0; j < r; j++)
-        {
             printf("%d  ", p[i].max[j]);
-        }
         printf("\t\t");
         for (j = 0; j < r; j++)
-        {
             printf("%d  ", p[i].need[j]);
-        }
         printf("\n");
     }
 
-    // Determine Current State in Safe State
     safeState();
-    int ch = 1;
-    do
-    {
-        printf("Request new resourse ?[0/1] :");
-        scanf("%d", &ch);
-        if (ch)
-            reqRes();
-    } while (ch != 0);
-
-    // end:printf("\n");
+    return 0;
 }
