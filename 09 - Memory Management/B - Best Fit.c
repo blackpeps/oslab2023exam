351b93cd1550a6ccc413adff74cbdfa0b2c8970a
blackpeps <126700907+blackpeps@users.noreply.github.com>
2023-08-09 23:59:23 +0530

    Updated appearance, functionality remains the same

--- 09 - Memory Management/B - Best Fit.c
+++ 09 - Memory Management/B - Best Fit.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
-#define MAX 20
+#define MAX 10
 int main()
 {
-    int bsize[MAX], fsize[MAX], nb, nf;
-    int temp, low = 10000;
-    static int bflag[MAX], fflag[MAX];
-    int i, j;
-    printf("\nEnter the number of blocks:");
-    scanf("%d", &nb);
-    printf("Enter the size of memory block");
-    for (i = 1; i <= nb; i++)
+    int bsize[MAX], psize[MAX], pno, bno, temp, low;
+    static int flag[MAX], allocation[MAX];
+    printf("Enter no. of blocks: ");
+    scanf("%d", &bno);
+    printf("Enter size of each block:\t");
+    for (int i = 0; i < bno; i++)
         scanf("%d", &bsize[i]);
-    printf("\nEnter the number of files");
-    scanf("%d", &nf);
-    printf("\nEnter the size of the file %d", i);
-    for (i = 1; i <= nf; i++)
-        scanf("%d", &fsize[i]);
-    for (i = 1; i <= nf; i++)
+    printf("Enter no. of process: ");
+    scanf("%d", &pno);
+    printf("Enter size of each process:\t");
+    for (int i = 0; i < pno; i++)
+        scanf("%d", &psize[i]);
+    printf("\nProcess No.\tSize\t\tBlock No\tSize\t\tRemaining\n");
+    for (int i = 0; i < pno; i++)
     {
-        for (j = 1; j <= nb; j++)
-        {
-            if (bflag[j] != 1)
+        low = 10000;
+        allocation[i] = -1;
+        for (int j = 0; j < bno; j++)
+            if (flag[j] != 1)
             {
-                temp = bsize[j] - fsize[i];
-                if (temp >= 0)
+                temp = bsize[j] - psize[i];
+                if (temp >= 0 && temp < low)
                 {
-                    if (low > temp)
-                    {
-                        fflag[i] = j;
-                        low = temp;
-                    }
+                    allocation[i] = j;
+                    low = temp;
                 }
             }
+        if (allocation[i] != -1)
+        {
+            printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, psize[i], allocation[i] + 1, bsize[allocation[i]], low);
+            flag[allocation[i]] = 1;
         }
-        bflag[fflag[i]] = 1;
-        low = 10000;
     }
-    printf("\nFile no.\tFile size\tBlock no\tBlock size");
-    for (i = 1; i <= nf; i++)
-        printf("\n\n%d\t\t%d\t\t%d\t\t%d", i, fsize[i], fflag[i], bsize[fflag[i]]);
+    return 0;
 }
